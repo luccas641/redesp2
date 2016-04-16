650988cd3009feb8a76e781f9563947096a8ff7f
luccas <luccas@vpeventos.com>
2016-04-15 22:47:26 -0300

    .

--- hello/views.py
+++ hello/views.py
@@ -58,44 +58,27 @@ def auth(request):
 def postChoice(request):
     global i
     global player
+    p = int(request.GET['p'])-1
 
-    requestLock()
-    player = (player +1) %2
-    i = request.GET['i']
-    releaseLock()
+    if(p==player):
+        player = (player +1) %2
+        i = request.GET['i']
+
     return JsonResponse({'msg': "ok"})
 
 
 def getChoice(request):
     global n
 
-    requestLock()
     p = int(request.GET['p'])-1
 
     if(n==True):
-        releaseLock()
         return JsonResponse({'player': 2})
 
     if(p==player):
-        releaseLock()
         return JsonResponse({'id': i})
 
-    releaseLock()
     return JsonResponse({'msg': "error"})
-
-def requestLock():
-    global lock
-
-    while(lock==True):
-        pass
-    lock = True
-
-def releaseLock():
-    global lock
-
-    while(lock==False):
-        pass
-    lock = False
 
 def new(request):
     global player
