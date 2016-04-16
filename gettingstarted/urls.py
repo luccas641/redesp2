9c3cf78f9a8630d9924c9301f0152b993b46ec77
luccas <luccas@vpeventos.com>
2016-04-15 22:09:30 -0300

    teste em url diferente

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^game$', hello.views.index, name='index'),
     url(r'^auth', hello.views.auth, name='auth'),
     url(r'^postChoice', hello.views.postChoice, name='postChoice'),
     url(r'^getChoice', hello.views.getChoice, name='getChoice'),
